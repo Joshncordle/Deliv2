a092ae798477dadc9cea1b45035457d9afdcd4b3
Josh Cordle <joshncordle@gmail.com>
2019-01-24 17:30:28 -0500

    Version 2

--- Deliv2/Program.cs
+++ Deliv2/Program.cs
@@ -22,17 +22,29 @@ namespace IterativeStatement
                 //name the variable 
                 int number = int.Parse(input);
 
-                //read the number entered by the user and the amount of times the loop will itereate
-                Console.WriteLine("You have entered " + number.ToString());
-
-                //Write the while loop
-                while (number > 0)
+                //Use if-else statement to ensure the number entered is less than 100 and more than 0
+                if ((number > 100) || (number < 0))
                 {
-                    Console.WriteLine("This is the current integer value in the loop: " + number.ToString());
-                    number--;
+                    Console.WriteLine("Please enter a number less than 100 and more than 1...");
+                    Console.WriteLine("Press any key to exit the program and try again...");
+                    Console.ReadKey(true);
                 }
-                Console.WriteLine("Press any key to close the program...");
-                Console.ReadKey(true);
+               
+                //Reacall else statement to preform loop if proper number is entered
+                else
+                {
+                    //read the number entered by the user and the amount of times the loop will itereate
+                    Console.WriteLine("You have entered " + number.ToString());
+
+                    //Write the while loop
+                    while (number > 0)
+                    {
+                        Console.WriteLine("This is the current integer value in the loop: " + number.ToString());
+                        number--;
+                    }
+                    Console.WriteLine("Press any key to close the program...");
+                    Console.ReadKey(true);
+                }
             }
             catch
             {
